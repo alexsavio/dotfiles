fc66ae0a693bb6f253734f6e549107cbae5b3bf8
Alexandre M. S <alexsavio@gmail.com>
2013-12-20 17:40:23 +0100

    Added Intel MKL configs. You should also add the lib paths to /etc/ld.so.conf.d/intel.conf

--- home/.bash.d/02_data_tools.bash
+++ home/.bash.d/02_data_tools.bash
@@ -4,32 +4,36 @@ export FSLPARALLEL=condor
 
 
 #FREESURFER
-export FREESURFER_HOME=/opt/freesurfer
-source $FREESURFER_HOME/SetUpFreeSurfer.sh
+if [ -d /opt/freesurfer ]; 
+then
+	export FREESURFER_HOME=/opt/freesurfer
+	source $FREESURFER_HOME/SetUpFreeSurfer.sh
+fi
 
 #CUDA
-export PATH=/usr/local/cuda/bin:$PATH
+if [ -d /usr/local/cuda/bin ]; 
+then
+	export PATH=/usr/local/cuda/bin:${PATH}
+fi
 
 #AFNI
-export PATH=~/abin:$PATH
+if [ -d ~/abin ]; 
+then
+	export PATH=~/abin:${PATH}
+fi
 
 #CVIP
-CVIPHOME=/home/alexandre/Software/cvip 
-CVIP_IMGPATH=./
-CVIP_DISPLAY=picture
-TCL_LIBRARY=/home/alexandre/Software/cvip/CVIPTCL/lib/tcl8.0
-TK_LIBRARY=/home/alexandre/Software/cvip/CVIPTCL/lib/tk8.0
-XF_LOAD_PATH=/home/alexandre/Software/cvip/CVIPTCL/GUI_SCRIPTS
-LD_LIBRARY_PATH=/usr/lib/fsl/5.0:/usr/lib/fsl/5.0:/home/alexandre/Software/cvip/lib:/home/alexandre/Software/cvip/CVIPTCL/lib
-PATH=${PATH}:/home/alexandre/Software/cvip/CVIPTCL:/home/alexandre/Software/cvip/bin
-export CVIPHOME
-export CVIP_IMGPATH
-export CVIP_DISPLAY
-export TCL_LIBRARY
-export TK_LIBRARY
-export XF_LOAD_PATH
-export LD_LIBRARY_PATH
-export PATH
+if [ -d /home/alexandre/Software/cvip ]; 
+then
+	export CVIPHOME=/home/alexandre/Software/cvip 
+	export CVIP_IMGPATH=./
+	export CVIP_DISPLAY=picture
+	export TCL_LIBRARY=/home/alexandre/Software/cvip/CVIPTCL/lib/tcl8.0
+	export TK_LIBRARY=/home/alexandre/Software/cvip/CVIPTCL/lib/tk8.0
+	export XF_LOAD_PATH=/home/alexandre/Software/cvip/CVIPTCL/GUI_SCRIPTS
+	export LD_LIBRARY_PATH=/usr/lib/fsl/5.0:/home/alexandre/Software/cvip/lib:/home/alexandre/Software/cvip/CVIPTCL/lib:${LD_LIBRARY_PATH}
+	export PATH=${PATH}:/home/alexandre/Software/cvip/CVIPTCL:/home/alexandre/Software/cvip/bin
+fi
 
 #virtualenvwrapper
 export WORKON_HOME=~/envs
@@ -40,4 +44,12 @@ export PIP_REQUIRE_VIRTUALENV=true
 #Python
 #git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
 source ~/.autoenv/activate.sh
+
+#Intel C++ Studio
+if [ -d /opt/intel ]; 
+then
+	export INTEL_HOME=/opt/intel
+	export PATH=${PATH}:${INTEL_HOME}/bin
+	export LD_LIBRARY_PATH=/opt/intel/composer_xe_2013/mkl/lib/intel64:/opt/intel/composer_xe_2013/lib/intel64:${LD_LIBRARY_PATH}
+fi
 
