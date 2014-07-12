9c880d767b038294fb24f51e5045d7080d9e1f43
Alexandre M. S <alexsavio@gmail.com>
2014-07-12 15:37:33 +0200

    Change from ssh-agent-find.sh to keychain

--- home/.bash.d/03_others.bash
+++ home/.bash.d/03_others.bash
@@ -1,7 +1,8 @@
 
 #export SSH_AUTH_SOCK=0
-source ~/bin/ssh-find-agent.sh
-set_ssh_agent_socket
+#source ~/bin/ssh-find-agent.sh
+#set_ssh_agent_socket
+eval `keychain --eval id_rsa`
 
 export CUDA_VISIBLE_DEVICES="1"
 
