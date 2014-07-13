31342158f9695812bf310256b75cc03079cadda1
Alexandre M. S <alexsavio@gmail.com>
2014-07-13 10:52:40 +0200

    Fix keychain to work with git

--- home/.bash.d/03_others.bash
+++ home/.bash.d/03_others.bash
@@ -2,7 +2,15 @@
 #export SSH_AUTH_SOCK=0
 #source ~/bin/ssh-find-agent.sh
 #set_ssh_agent_socket
-eval `keychain --eval id_rsa`
+
+### START-Keychain ###
+# Let  re-use ssh-agent and/or gpg-agent between logins
+alias ssh='eval $(/usr/bin/keychain --eval --agents ssh -Q --quiet ~/.ssh/id_rsa) && ssh'
+alias git='eval $(/usr/bin/keychain --eval --agents ssh -Q --quiet ~/.ssh/id_rsa) && git'
+
+#eval $(keychain --clear --eval id_rsa)
+#source $HOME/.keychain/$HOSTNAME-sh
+### End-Keychain ###
 
 export CUDA_VISIBLE_DEVICES="1"
 
