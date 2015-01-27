ac24d111f1da46ed4c14d03e04f3b02535fcbaff
Alexandre M. S <alexsavio@gmail.com>
2015-01-27 13:13:53 +0100

    Fix things for OSX

--- home/.zsh.d/00_general_vars.zsh
+++ home/.zsh.d/00_general_vars.zsh
@@ -1,11 +1,15 @@
 #!/usr/bin/env zsh
 
- 
+
 #HOMESCHICK
 #git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 source "$HOME/.homesick/repos/homeshick/homeshick.sh"
-#fpath=("${ZDOTDIR:-$HOME}/.homesick/repos/homeshick/completions" $fpath)
-#source "$HOME/.homesick/repos/homeshick/completions/homeshick-completion.bash"
+fpath=($HOME/.homesick/repos/homeshick/completions $fpath)
+compinit
+
+#PYTHON
+export PYTHON2_PATH=/Library/Frameworks/Python.framework/Versions/2.7
+export PYTHON3_PATH=/Library/Frameworks/Python.framework/Versions/3.4
 
 # Your place for hosting Git repos. I use this for private repos.
 export GIT_HOSTING='git@github.alexsavio.com'
@@ -32,77 +36,76 @@ export TODO="t"
 
 # why would you type 'cd dir' if you could just type 'dir'?
 setopt AUTO_CD
- 
+
 # Now we can pipe to multiple outputs!
 setopt MULTIOS
- 
+
 # This makes cd=pushd
 setopt AUTO_PUSHD
- 
+
 # This will use named dirs when possible
 setopt AUTO_NAME_DIRS
- 
+
 # If we have a glob this will expand it
 setopt GLOB_COMPLETE
 setopt PUSHD_MINUS
- 
+
 # No more annoying pushd messages...
 # setopt PUSHD_SILENT
- 
+
 # blank pushd goes to home
 setopt PUSHD_TO_HOME
- 
+
 # this will ignore multiple directories for the stack. Useful? I dunno.
 setopt PUSHD_IGNORE_DUPS
- 
+
 # 10 second wait if you do something that will delete everything. I wish I'd had this before...
 setopt RM_STAR_WAIT
- 
+
 # use magic (this is default, but it can't hurt!)
 setopt ZLE
- 
+
 setopt NO_HUP
- 
+
 # only fools wouldn't do this ;-)
 export EDITOR="/usr/bin/vim"
- 
+
 setopt IGNORE_EOF
- 
+
 # If I could disable Ctrl-s completely I would!
 setopt NO_FLOW_CONTROL
- 
+
 # Keep echo "station" > station from clobbering station
 setopt NO_CLOBBER
- 
+
 # Case insensitive globbing
 setopt NO_CASE_GLOB
- 
+
 # Be Reasonable!
 setopt NUMERIC_GLOB_SORT
- 
+
 # I don't know why I never set this before.
 setopt EXTENDED_GLOB
- 
+
 # hows about arrays be awesome? (that is, frew${cool}frew has frew surrounding all the variables, not just first and last
 setopt RC_EXPAND_PARAM
- 
+
 # Who doesn't want home and end to work?
 bindkey '\e[1~' beginning-of-line
 bindkey '\e[4~' end-of-line
- 
+
 # Incremental search is elite!
 bindkey -M vicmd "/" history-incremental-search-backward
 bindkey -M vicmd "?" history-incremental-search-forward
- 
+
 # Search based on what you typed in already
 bindkey -M vicmd "//" history-beginning-search-backward
 bindkey -M vicmd "??" history-beginning-search-forward
- 
+
 bindkey "\eOP" run-help
- 
+
 # oh wow! This is killer... try it!
 bindkey -M vicmd "q" push-line
- 
+
 # it's like, space AND completion. Gnarlbot.
 bindkey -M viins ' ' magic-space
-
