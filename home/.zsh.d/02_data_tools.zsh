33d0a29a92ec32efe31338699250ba823ba31be6
Alexandre M. S <alexsavio@gmail.com>
2015-02-03 20:18:56 +0100

    Add more tools to 02_data_tools

--- home/.zsh.d/02_data_tools.zsh
+++ home/.zsh.d/02_data_tools.zsh
@@ -41,10 +41,18 @@ fi
 addapath /opt/Komodo-Edit/bin
 
 #CUDA
-addapath /usr/local/cuda/bin
+if [ -d /usr/local/cuda ];
+then
+    addapath /usr/local/cuda/bin
+fi
 
 #AFNI
-addapath ~/abin
+if [ -d ~/abin ]
+then 
+    export AFNI_PATH=~/abin
+    addapath $AFNI_PATH
+    export DYLD_FALLBACK_LIBRARY_PATH=$AFNI_PATH
+fi
 
 #CVIP
 if [ -d ~/Software/cvip ];
@@ -59,6 +67,20 @@ then
     export PATH=${PATH}:~/Software/cvip/CVIPTCL:~/Software/cvip/bin
 fi
 
+#ANTS
+if [ -d ~/Software/ants ];
+then
+    export ANTSPATH=~/Software/ants/build/bin
+    addapath $ANTSPATH
+fi
+
+#C3D
+if [ -d ~/Software/c3d ];
+then
+    export C3D_PATH=~/Software/c3d/bin
+    addapath $C3D_PATH
+fi
+
 #Python
 if [[ "$(uname -s)" == 'Darwin' ]]
 then
@@ -70,7 +92,7 @@ else
 fi
 
 
-#virtualenvwrapper
+# VIRTUALENVWRAPPER
 if [ -x `which virtualenvwrapper.sh` ]
 then
     export WORKON_HOME=~/envs
@@ -78,14 +100,20 @@ then
 #    export PIP_REQUIRE_VIRTUALENV=true
 fi
 
+# PIP
+export PIP_CONFIG_FILE=~/.pip/pip.conf
+# pip zsh completion start
+function _pip_completion {
+  local words cword
+  read -Ac words
+  read -cn cword
+  reply=( $( COMP_WORDS="$words[*]" \
+             COMP_CWORD=$(( cword-1 )) \
+             PIP_AUTO_COMPLETE=1 $words[1] ) )
+}
+compctl -K _pip_completion pip
+# pip zsh completion end
 
-#autoenv
-#Python
-#git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
-#source ~/.autoenv/activate.sh
-
-#python-jedi
-#export PYTHONSTARTUP=~/.pythonrc.py
 
 #Intel C++ Studio
 if [ -d /opt/intel ];
