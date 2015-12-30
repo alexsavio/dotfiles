9a3c8dbf26d75ab45bd474309ef2a52fee422d37
Alexandre M. S <alexsavio@gmail.com>
2015-12-30 15:45:12 +0100

    Add Matlab path to Mac OS

--- home/.zsh.d/04_mac.zsh
+++ home/.zsh.d/04_mac.zsh
@@ -13,5 +13,10 @@ if [ -d /Applications/Juno.app/ ]; then
     addapath /Applications/Juno.app/Contents/Resources/app/julia/bin
 fi
 
+# Matlab
+if [ -d /Applications/MATLAB_R2015b.app/ ]; then
+    addapath /Applications/MATLAB_R2015b.app/bin
+fi
+
 export LC_ALL=en_GB.UTF-8
 export LANG=en_GB.UTF-8
