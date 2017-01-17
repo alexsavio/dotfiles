5c5a02f5fe8273bfbf72e14cbed14314e441b16a
Alexandre Manhaes Savio <alexsavio@gmail.com>
2017-01-17 14:32:09 +0100

    add init.sh

---
+++ home/.zsh.d/init.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo '00_general_vars.zsh'
+echo '01_aliases.zsh'
+echo '02_data_tools.zsh'
+echo '03_neuro.zsh'
+echo '03_others.zsh'
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform
+    echo '04_mac.zsh'
+    echo '04_xmodmap.zsh'
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+    echo '04_ubuntu.zsh'
+#elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under Windows NT platform
+fi
