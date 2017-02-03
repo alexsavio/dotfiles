e0168d42eddc450b0307f726d2ae582277897559
Alexandre Manhaes Savio <alexsavio@gmail.com>
2017-02-03 11:10:22 +0100

    add git aliases

--- home/.zsh.d/01_aliases.zsh
+++ home/.zsh.d/01_aliases.zsh
@@ -75,6 +75,16 @@ if ( isinpath vagrant ); then
 	alias vstat="vagrant status" 
 fi
 
+# git aliases
+if ( isinpath git ); then
+    git config --global alias.co checkout
+    git config --global alias.br branch
+    git config --global alias.ci commit
+    git config --global alias.st status
+    git config --global alias.unstage 'reset HEAD --'
+    git config --global alias.last 'log -1 HEAD'
+fi
+
 #folders gotos
 alias mydocs='cd ~/Documents'
 alias mydowns='cd ~/Downloads'
