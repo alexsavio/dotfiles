007c36df9a99ee21b33bef07c5ae59037d65beee
Alexandre M. Savio <alexsavio@gmail.com>
2018-04-25 21:22:10 +0200

    feature(aliases): fix, and add aliases for fasd

--- home/.zsh.d/02_aliases.zsh
+++ home/.zsh.d/02_aliases.zsh
@@ -37,6 +37,18 @@ if ( isinpath vagrant ); then
 	alias vburn="vagrant destroy -f"
 fi
 
+#vagrant aliases
+if ( isinpath fasd ); then
+    alias a='fasd -a'        # any
+    alias s='fasd -si'       # show / search / select
+    alias d='fasd -d'        # directory
+    alias f='fasd -f'        # file
+    alias sd='fasd -sid'     # interactive directory selection
+    alias sf='fasd -sif'     # interactive file selection
+    alias z='fasd cd -d'     # cd, same functionality as j in autojump
+    alias zz='fasd cd -d -i' # cd with interactive selection
+fi
+
 # git aliases
 if ( isinpath git ); then
     git config --global alias.co checkout
@@ -53,11 +65,11 @@ alias mydowns='cd ~/Downloads'
 alias mydesk='cd ~/Desktop'
 alias mymusic='cd ~/Music'
 alias mydbox='cd ~/Dropbox'
-alias myprojs='cd ~/Projects'
+alias myprojs='cd ~/projects'
 
 #folders hashes
 hash -d dbox='~/Dropbox'
-hash -d prjs='~/Projects'
+hash -d prjs='~/projects'
 
 alias please='sudo'
 
@@ -69,9 +81,8 @@ alias annex='git annex'
 # See /usr/share/doc/bash-doc/examples in the bash-doc package.
 
 alias jupy="jupyter console"
-alias jupynb="jupyter notebook"
-
-alias ipy="python -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
+alias junb="jupyter notebook"
+alias julab="jupyter lab"
 
 #docker aliases
 if ( isinpath docker ); then
