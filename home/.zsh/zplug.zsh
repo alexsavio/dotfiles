54600f7ff5aaa0db605602e9e7ff1d880abb569a
Alexandre M. Savio <alexsavio@gmail.com>
2018-04-29 19:35:53 -0300

    fix(zplug.zsh): fix zplug config

--- home/.zsh/zplug.zsh
+++ home/.zsh/zplug.zsh
@@ -2,6 +2,27 @@
 
 zplug "zplug/zplug", hook-build: "zplug --self-manage"
 
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/github", from:oh-my-zsh
+# zplug "plugins/fossil", from:oh-my-zsh
+
+zplug "plugins/docker", from:oh-my-zsh
+zplug "plugins/docker-compose", from:oh-my-zsh
+
+zplug "plugins/python", from:oh-my-zsh
+zplug "plugins/pip", from:oh-my-zsh
+
+zplug "plugins/autojump", from:oh-my-zsh
+zplug "plugins/command-not-found", from:oh-my-zsh
+zplug "plugins/gpg-agent", from:oh-my-zsh
+zplug "plugins/ssh-agent", from:oh-my-zsh
+zplug "plugins/thefuck", from:oh-my-zsh
+
+zplug "plugins/aws", from:oh-my-zsh
+zplug "plugins/osx", from:oh-my-zsh
+zplug "plugins/battery", from:oh-my-zsh
+
+
 # zplug "b4b4r07/enhancd", use:init.sh
 # if zplug check "b4b4r07/enhancd"; then
 #     #export ENHANCD_FILTER="fzf --height 50% --reverse --ansi --preview 'ls -l {}' --preview-window down"
@@ -15,9 +36,6 @@ zplug "zsh-users/zsh-syntax-highlighting", defer:3, on:"zsh-users/zsh-autosugges
 zplug "zsh-users/zsh-history-substring-search", defer:3, on:"zsh-users/zsh-syntax-highlighting"
 
 zplug "glidenote/hub-zsh-completion"
-# zplug 'Valodim/zsh-curl-completion'
-
-zplug "plugins/git", from:oh-my-zsh, as:plugin
 
 zplug "b4b4r07/enhancd", use:enhancd.sh
 zplug 'b4b4r07/fzf-powertools', \
@@ -47,6 +65,6 @@ zplug "wbingli/zsh-wakatime", from:github
 zplug "~/.zsh.d", from:local, use:"<->_*.zsh"
 
 zplug "mafredri/zsh-async", from:github, defer:0
-# zplug 'knu/zsh-manydots-magic', use:manydots-magic, defer:2
+zplug 'knu/zsh-manydots-magic', use:manydots-magic, defer:3
 
 zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
