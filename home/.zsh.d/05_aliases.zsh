06ed619dec5954539163ad3ef4761c9c1b70f826
alexsavio <alexsavio@gmail.com>
2026-05-22 20:07:33 +0200

    yolo

--- home/.zsh.d/05_aliases.zsh
+++ home/.zsh.d/05_aliases.zsh
@@ -8,13 +8,6 @@ if (( $+commands[eza] )); then
   alias lt='eza -T --git-ignore --level=2 --group-directories-first'
   alias llt='eza -lT --git-ignore --level=2 --group-directories-first'
   alias lT='eza -T --git-ignore --level=4 --group-directories-first'
-elif (( $+commands[exa] )); then
-  alias ls='exa'
-  alias l='exa -l --all --group-directories-first --git'
-  alias ll='exa -l --all --all --group-directories-first --git'
-  alias lt='exa -T --git-ignore --level=2 --group-directories-first'
-  alias llt='exa -lT --git-ignore --level=2 --group-directories-first'
-  alias lT='exa -T --git-ignore --level=4 --group-directories-first'
 else
   alias l='ls -lah'
   alias ll='ls -alF'
--- home/.zshrc
+++ home/.zshrc
@@ -10,3 +10,12 @@ if command -v wt >/dev/null 2>&1; then
   eval "$(command wt config shell init zsh)"
   alias wsc='wt switch --create --execute=claude'
 fi
+
+if command -v wt >/dev/null 2>&1; then eval "$(command wt config shell init zsh)"; fi
+
+# bun completions
+[ -s "/Users/alexandre/.bun/_bun" ] && source "/Users/alexandre/.bun/_bun"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
